4c87b2baafc95d847a5542f3a6119d72c74b87da
Koichi Yamashita <k-yamashita@ver2.jp>
2013-10-31 16:35:12 +0900

    ver

--- version.php
+++ version.php
@@ -3,5 +3,5 @@
 defined('MOODLE_INTERNAL') || die;
 
 $plugin->component = 'block_bayes';
-$plugin->version   = 2013103000;
+$plugin->version   = 2013103100;
 $plugin->requires  = 2012120300.00; // Moodle 2.4
